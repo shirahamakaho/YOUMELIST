967668f3e0600e8cd624e6706449464d1c6a898e
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-11-14 08:56:15 +0000

    [Add]favorite action

--- app/controllers/user/favorites_controller.rb
+++ app/controllers/user/favorites_controller.rb
@@ -1,2 +1,13 @@
 class User::FavoritesController < ApplicationController
+    def create
+        @comment_favorite = Favorite.new(user_id:current_user.id,comment_id:params[:comment_id])
+        @comment_favorite.save
+        redirect_to dream_path(params[:dream_id])
+    end
+
+    def destroy
+        @comment_favorite = Favorite.find_by(user_id:current_user.id,comment_id:params[:comment_id])
+        @comment_favorite.destroy
+        redirect_to dream_path(params[:dream_id])
+    end
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,8 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :dream
   has_many :favorites,dependent: :destroy
+  def favorited?(user)
+    favorites.where(user_id: user.id).exists?
+    # current_userがfavoriteしてる？
+  end
 end
