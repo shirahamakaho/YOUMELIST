d7f7e6772eb069cdbdd37024e8d26ab99bbfa7c5
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-11-19 14:41:10 +0000

    [update]layout

--- app/controllers/user/searches_controller.rb
+++ app/controllers/user/searches_controller.rb
@@ -2,6 +2,16 @@ class User::SearchesController < ApplicationController
   def search
     @range = params[:range]
     @word = params[:word]
-    @dreams = Dream.looks(params[:search],@word)
+    @searched_dreams = Dream.looks(params[:search],@word)
+    if params[:latest]
+      @dreams = Dream.latest
+    elsif params[:old]
+      @dreams = Dream.old
+    else
+      @dreams = Dream.all
+    end
+    @latestdream = Dream.limit(3).order("created_at DESC")
+    @latestcomment = Comment.limit(5).order("created_at DESC")
+    @list = List.new
   end
 end
