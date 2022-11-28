8fff58576199e5460b06cce581ddc05cc80a8422
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-11-28 07:27:58 +0000

    [update]adminデータ

--- db/schema.rb
+++ db/schema.rb
@@ -99,6 +99,7 @@ ActiveRecord::Schema.define(version: 2022_11_18_004633) do
     t.datetime "updated_at", precision: 6, null: false
     t.text "introduction"
     t.boolean "is_deleted", default: false
+    t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
