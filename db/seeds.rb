47af4f5fc441668f89517f54c19b6d18977a14fd
shirahamakaho <112605721+shirahamakaho@users.noreply.github.com>
2022-11-28 07:11:20 +0000

    adminデータ

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Admin.create :email => 'youmelist2022@pg1x.com', :password => 'chao0the'
